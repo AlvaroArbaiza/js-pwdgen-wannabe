e348ef1f9b6c37c0d269055af27aea5441e68220
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-03-21 16:03:03 +0100

    Struttura prompt e inserimento dati richiesti

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Problema:
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+const nome = prompt("Inserisci il tuo nome");
+const cognome = prompt("Inserisci il tuo cognome");
+const colorePreferito = prompt("Inserisci il tuo colore preferito");
