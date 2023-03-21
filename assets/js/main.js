46a4e7f659d4637a5211906eb319e1533a92d6d8
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-03-21 22:38:31 +0100

    Refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,15 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
-const nome = prompt("Inserisci il tuo nome");
+let nome = prompt("Inserisci il tuo nome");
 
-const cognome = prompt("Inserisci il tuo cognome");
+let cognome = prompt("Inserisci il tuo cognome");
 
-const colorePreferito = prompt("Inserisci il tuo colore preferito");
+let colorePreferito = prompt("Inserisci il tuo colore preferito");
 
-const password = nome + cognome + colorePreferito + 21;
+const endPassword = 21;
+
+let password = nome + cognome + colorePreferito + endPassword;
 
 document.getElementById("password").innerHTML = `
     <p>La tua Password è: ${password}</p>
